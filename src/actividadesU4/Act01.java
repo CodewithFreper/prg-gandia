f9536d42fda76ac1bb725a8a2e417c50c27aa589
CodewithFreper <codewithfreper@gmail.com>
2026-09-17 12:12:12 +0200

    Actividades 1 y 2 de la unidad 4 añadidas.

---
+++ src/actividadesU4/Act01.java
@@ -0,0 +1,8 @@
+/*
+ * Escribe un método que devuelva el mayor de tres números que se piden al usuario.
+ */
+
+package actividadesU4;
+
+public class Act01 {
+}
---
+++ src/actividadesU4/Act02.java
@@ -0,0 +1,9 @@
+/*
+ *Diseña un método que devuelva el mayor de cuatro números pasados como parámetros.
+ */
+
+package actividadesU4;
+
+public class Act02 {
+
+}
